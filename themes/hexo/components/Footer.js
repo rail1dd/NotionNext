1204a48c8d5cc1c41fea92883724197bef2fe16f
rail1dd <yangyummy@outlook.com>
2024-12-01 18:03:00 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -1,6 +1,5 @@
 import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import BeiAnSite from '@/components/BeiAnSite'
-import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
 
 const Footer = ({ title }) => {
@@ -11,14 +10,14 @@ const Footer = ({ title }) => {
     parseInt(since) < currentYear ? since + '-' + currentYear : currentYear
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'>
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6 text-gray-600 dark:text-gray-100 text-sm p-6'>
       {/* <DarkModeButton/> */}
       <i className='fas fa-copyright' /> {`${copyrightDate}`}
       <span>
         <i className='mx-1 animate-pulse fas fa-heart' />
         <a
           href={siteConfig('LINK')}
-          className='underline font-bold  dark:text-gray-300 '>
+          className='underline font-bold dark:text-gray-300'>
           {siteConfig('AUTHOR')}
         </a>
         .<br />
@@ -35,7 +34,17 @@ const Footer = ({ title }) => {
         <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>
           {title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}
         </h1>
-        <PoweredBy className='justify-center' />
+        {/* 替换了 PoweredBy 组件 */}
+        <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>
+          Powered by{' '}
+          <a href='https://www.cloudflare-cn.com/' target='_blank'>
+            Cloudflare
+          </a>{' '}
+          &{' '}
+          <a href='https://vercel.com' target='_blank'>
+            Vercel
+          </a>.
+        </p>
       </span>
       <br />
     </footer>
